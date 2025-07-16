2cf676b64dea5f1e0267bcee397df55402b3a337
Caleb C <wolfertamer@gmail.com>
2025-07-15 23:41:10 -0700

    Allowed for HTTPS

--- server/AbooOpenWhen/Program.cs
+++ server/AbooOpenWhen/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+                          policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials();
                       });
 });
 
